a641699511731ece92e5e2ca6bebc9a7bcf7d913
nelioyann <nanayannick1998@gmail.com>
2020-04-05 13:55:28 +0200

    oof

--- scripts/script.js
+++ scripts/script.js
@@ -165,3 +165,12 @@ function afficher_surnom() {
     });
 
 };
+
+const date_el = document.querySelector(".header_date")
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+date_el.innerHTML = today
