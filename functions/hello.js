28004a6aafabea3678b69043e6eb6797b1189471
nelioyann <nanayannick1998@gmail.com>
2020-04-19 21:37:35 +0200

    testing lambda

---
+++ functions/hello.js
@@ -0,0 +1,7 @@
+exports.handler = async (event, context) => {
+    const name = event.queryStringParameters.name || "World";
+    return {
+        statusCode: 200,
+        body: `Hello, ${name}`
+    };
+};
