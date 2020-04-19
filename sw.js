cd506abb1f187461d46e2060fe03ad835fccb2bd
nelioyann <nanayannick1998@gmail.com>
2020-04-19 13:07:14 +0200

    a

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
 // Label des fichiers statiques to precache
-const staticCacheName = "site-static-ve477"
+const staticCacheName = "site-static-v7"
 
 // Fichier qui seront cacher
 const assets = [
